d170a816a2a8429214a927445b0d30646848ad6d
Airton Moreira <est.moreira.itzel@latecnicalf.com.ar>
2026-08-20 19:40:17 -0300

    Creacion de supabase

---
+++ mila 1/js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "TU_PROJECT_URL";
+const SUPABASE_KEY = "// sb_publishable_SJljTLi-mSxCZYKhU6xreg_KTbS_7rV"; // sb_publishable_SJljTLi-mSxCZYKhU6xreg_KTbS_7rV
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
