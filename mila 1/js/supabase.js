854804f7a20f8afdc33e7bc732f1c503582e90bc
Airton Moreira <est.moreira.itzel@latecnicalf.com.ar>
2026-09-10 17:07:53 -0300

    Primera parte base de datos

--- mila 1/js/supabase.js
+++ mila 1/js/supabase.js
@@ -1,7 +1,24 @@
-const SUPABASE_URL = "TU_PROJECT_URL";
-const SUPABASE_KEY = "// sb_publishable_SJljTLi-mSxCZYKhU6xreg_KTbS_7rV"; // sb_publishable_SJljTLi-mSxCZYKhU6xreg_KTbS_7rV
+const SUPABASE_URL = "https://rvssvunhrgnbmdkwtvax.supabase.co";
+const SUPABASE_KEY = "sb_publishable_SJljTLi-mSxCZYKhU6xreg_KTbS_7rV"; // sb_publishable_SJljTLi-mSxCZYKhU6xreg_KTbS_7rV
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+async function probarSupabase() {
+    const { data, error } = await supabaseClient
+        .from("negocios")
+        .select("id, nombre, tipo_negocio")
+        .limit(1);
+
+    if (error) {
+        console.error("Error conectando con Supabase:", error);
+        return;
+    }
+
+    console.log("Supabase conectado correctamente:");
+    console.log(data);
+}
+
+probarSupabase();
